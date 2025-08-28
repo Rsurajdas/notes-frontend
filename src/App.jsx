b87659136e9e93716104b3cf2a97ded356a61013
Suraj Kumar <suraj.k@avantaifintech.com>
2025-08-28 21:29:49 +0530

    feat: Button component created as per figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Button from "./components/Buttons/Button";
+import { Restore } from "./icons/Restore";
 
 const router = createBrowserRouter([
   {
@@ -7,6 +9,11 @@ const router = createBrowserRouter([
     element: (
       <div className="text-preset-1 p-brand-25">
         The quick brown fox jumps over the lazy dog.
+        <div className="flex flex-col gap-4 items-start mt-4">
+          <Button variant="primary" text="Primary Button" />
+          <Button variant="secondary" text="Secondary Button" />
+          <Button variant="border" icon={Restore} text="Border Button" />
+        </div>
       </div>
     ),
   },
---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,28 @@
+export default function Button({
+  text,
+  variant = "primary",
+  icon: Icon,
+  ...props
+}) {
+  const styles =
+    variant === "primary"
+      ? "bg-custom-blue-500 text-custom-neutral-0 hover:bg-custom-blue-700 focus:shadow-custom-focus"
+      : variant === "secondary"
+      ? "bg-custom-neutral-100 text-custom-neutral-600 hover:text-custom-neutral-950 hover:bg-custom-neutral-0 hover:outline hover:outline-offset-[-1px] hover:outline-custom-neutral-300 focus:shadow-custom-focus focus:outline focus:outline-offset-[-1px] focus:outline-custom-neutral-950 focus:bg-custom-neutral-0 focus:text-custom-neutral-950"
+      : "bg-transparent text-custom-neutral-950 outline outline-custom-neutral-300 outline-offset-[-1px] hover:bg-custom-neutral-100 hover:text-custom-neutral-600 hover:outline-none focus:outline focus:shadow-custom-focus focus:outline-offset-[-1px] focus:outline-custom-neutral-950 focus:bg-custom-neutral-0 focus:text-custom-neutral-950";
+  return (
+    <button
+      {...props}
+      className={`inline-flex cursor-pointer rounded-custom-8 px-custom-200 py-custom-150 text-preset-4 disabled:bg-custom-neutral-100 disabled:text-custom-neutral-300 disabled:cursor-not-allowed disabled:outline-none items-center gap-2 group ${styles}`}
+    >
+      <>
+        {Icon && (
+          <span>
+            <Icon width="20" height="20" />
+          </span>
+        )}
+        <span>{text}</span>
+      </>
+    </button>
+  );
+}
