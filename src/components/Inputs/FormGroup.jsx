78b5939160d8866d6da39e2a4f059705cca7779c
Suraj Kumar <suraj.k@avantaifintech.com>
2025-09-06 15:11:28 +0530

    feat: added search icon in form group

--- src/components/Inputs/FormGroup.jsx
+++ src/components/Inputs/FormGroup.jsx
@@ -3,6 +3,7 @@ import { InfoCircle } from "../../icons/InfoCircle";
 import { ShowPassword } from "../../icons/ShowPassword";
 import { HidePassword } from "../../icons/HidePassword";
 import { Link } from "react-router";
+import { Search } from "../../icons/Search";
 
 export default function FormGroup({
   type = "text",
@@ -15,36 +16,44 @@ export default function FormGroup({
   hasLink = false,
   hintText,
   error,
+  className,
 }) {
   const [show, setShow] = useState(false);
   return (
     <div className="gap-custom-75 flex flex-col">
-      <div className="flex items-center justify-between">
-        <label
-          htmlFor={htmlFor}
-          className="text-preset-4 text-custom-neutral-950"
-        >
-          {label}
-        </label>
-        {hasLink ? (
-          <Link
-            to="forgot-password"
-            className="font-inter text-custom-neutral-600 text-right text-sm/[1.4] hover:underline"
+      {label ? (
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor={htmlFor}
+            className="text-preset-4 text-custom-neutral-950"
           >
-            Forgot
-          </Link>
-        ) : null}
-      </div>
+            {label}
+          </label>
+          {hasLink ? (
+            <Link
+              to="forgot-password"
+              className="font-inter text-custom-neutral-600 text-right text-sm/[1.4] hover:underline"
+            >
+              Forgot
+            </Link>
+          ) : null}
+        </div>
+      ) : null}
       <div className="relative w-full">
+        {type === "search" ? (
+          <span className="left-custom-200 absolute top-1/2 translate-y-[-50%]">
+            <Search width="20" height="20" color="#717784" />
+          </span>
+        ) : null}
         <input
           type={show ? "text" : type}
           placeholder={placeholder}
           id={id}
           name={name}
           disabled={disabled}
-          className={`bg-custom-neutral-0 px-custom-200 py-custom-150 rounded-custom-8 placeholder:font-inter placeholder:text-custom-neutral-500 text-preset-5 text-custom-neutral-950 focus:shadow-custom-focus-input focus:outline-custom-neutral-950 hover:bg-custom-neutral-50 hover:focus:bg-custom-neutral-0 disabled:bg-custom-neutral-50 disabled:placeholder:text-custom-neutral-300 disabled:text-custom-neutral-300 w-full text-sm/[1.3] outline outline-offset-[-1px] placeholder:text-sm/[1.3] placeholder:font-normal placeholder:tracking-[-0.2px] disabled:cursor-not-allowed ${
+          className={`bg-custom-neutral-0 px-custom-200 py-custom-150 rounded-custom-8 placeholder:font-inter placeholder:text-custom-neutral-500 text-preset-5 text-custom-neutral-950 focus:shadow-custom-focus-input focus:outline-custom-neutral-950 hover:bg-custom-neutral-50 hover:focus:bg-custom-neutral-0 disabled:bg-custom-neutral-50 disabled:placeholder:text-custom-neutral-300 disabled:text-custom-neutral-300 text-sm/[1.3] outline outline-offset-[-1px] placeholder:text-sm/[1.3] placeholder:font-normal placeholder:tracking-[-0.2px] disabled:cursor-not-allowed ${
             error ? "outline-custom-red-500" : "outline-custom-neutral-300"
-          }`}
+          } ${type === "search" ? "pl-[2.75rem]" : ""} ${className || ""}`}
         />
         {type === "password" ? (
           <button
