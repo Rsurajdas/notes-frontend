4ad4b800bd47594206546fb47b3797d908d687c0
Suraj Kumar <suraj.k@avantaifintech.com>
2025-09-06 15:10:57 +0530

    fix: Button component modified to use it as link also

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router";
+
 export default function Button({
   text,
   variant = "primary",
   icon: Icon,
   className,
+  to,
   ...props
 }) {
+  const Type = to ? Link : "button";
   const styles =
     variant === "primary"
       ? "bg-custom-blue-500 text-custom-neutral-0 hover:bg-custom-blue-700 focus:shadow-custom-focus"
@@ -12,9 +16,10 @@ export default function Button({
         ? "bg-custom-neutral-100 text-custom-neutral-600 hover:text-custom-neutral-950 hover:bg-custom-neutral-0 hover:outline hover:outline-offset-[-1px] hover:outline-custom-neutral-300 focus:shadow-custom-focus focus:outline focus:outline-offset-[-1px] focus:outline-custom-neutral-950 focus:bg-custom-neutral-0 focus:text-custom-neutral-950"
         : "bg-custom-neutral-0 text-custom-neutral-950 outline outline-custom-neutral-300 outline-offset-[-1px] hover:bg-custom-neutral-100 hover:text-custom-neutral-600 hover:outline-custom-neutral-100 hover:focus:bg-custom-neutral-0 hover:focus:outline-custom-neutral-950 focus:shadow-custom-focus focus:outline-custom-neutral-950 focus:text-custom-neutral-950";
   return (
-    <button
+    <Type
       {...props}
       className={`rounded-custom-8 px-custom-200 py-custom-150 disabled:bg-custom-neutral-100 disabled:text-custom-neutral-300 group inline-flex cursor-pointer items-center justify-center gap-2 disabled:cursor-not-allowed disabled:outline-none ${className} ${styles}`}
+      to={to}
     >
       <>
         {Icon && (
@@ -28,6 +33,6 @@ export default function Button({
         )}
         <span>{text}</span>
       </>
-    </button>
+    </Type>
   );
 }
