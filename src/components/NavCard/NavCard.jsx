378e58fcc1f737663cda80bf2b0a54b2804e601a
Suraj Kumar <suraj.k@avantaifintech.com>
2025-09-08 21:52:31 +0530

    feat: NavCard is card along with NavCardList and added it into the Authenticated layout

---
+++ src/components/NavCard/NavCard.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router";
+
+export default function NavCard({ item }) {
+  return (
+    <NavLink
+      to={`notes/${item.id}`}
+      className={({ isActive }) =>
+        `gap-custom-50 hover:bg-custom-neutral-100 rounded-custom-6 group flex flex-col ${isActive ? "bg-custom-neutral-100" : ""}`
+      }
+    >
+      {({ isActive }) => (
+        <>
+          <div className="gap-custom-150 p-custom-100 flex flex-col">
+            <h3 className="text-preset-3">{item.title}</h3>
+            <div className="gap-custom-50 flex items-center">
+              {item.tags.map((tag, idx) => (
+                <span
+                  key={idx}
+                  className="rounded-custom-4 bg-custom-neutral-200 py-custom-25 px-custom-75 text-preset-6"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <p className="text-preset-6 text-custom-neutral-700">{item.date}</p>
+          </div>
+          <hr
+            className={`rounded-custom-20 border-0 border-b group-hover:border-transparent ${isActive ? "border-transparent" : "border-custom-neutral-200"}`}
+          />
+        </>
+      )}
+    </NavLink>
+  );
+}
--- src/components/layouts/AuthenticatedLayout.jsx
+++ src/components/layouts/AuthenticatedLayout.jsx
@@ -2,7 +2,40 @@ import { Outlet } from "react-router";
 import Sidebar from "../Sidebar/SideBar";
 import MainHeader from "../MainHeader/MainHeader";
 import Button from "../Buttons/Button";
+import NavCardList from "../NavCard/NavCardList";
 
+const data = [
+  {
+    id: 1,
+    title: "React Performance Optimization",
+    tags: ["Dev", "React"],
+    date: "2023-10-01",
+  },
+  {
+    id: 2,
+    title: "Understanding JavaScript Closures",
+    tags: ["JavaScript", "Programming"],
+    date: "2023-09-28",
+  },
+  {
+    id: 3,
+    title: "CSS Grid vs. Flexbox: When to Use Which",
+    tags: ["CSS", "Design"],
+    date: "2023-09-25",
+  },
+  {
+    id: 4,
+    title: "A Guide to Modern Web Accessibility",
+    tags: ["Accessibility", "Web"],
+    date: "2023-09-20",
+  },
+  {
+    id: 5,
+    title: "State Management in React: Context API vs Redux",
+    tags: ["React", "State Management"],
+    date: "2023-09-15",
+  },
+];
 export default function AuthenticatedLayout() {
   return (
     <main className="bg-custom-neutral-0 flex h-full min-h-screen">
@@ -17,8 +50,11 @@ export default function AuthenticatedLayout() {
               to="notes/new"
               className=""
             />
-            <Outlet />
+            <>
+              <NavCardList data={data} />
+            </>
           </div>
+          <Outlet />
         </section>
       </section>
     </main>
